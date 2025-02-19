8bead2ae5975e9ab927f3efe6703fee0559b1b97
Eladiia <123058893+Eladiia@users.noreply.github.com>
2025-02-19 03:31:04 -0600

    Se hace la creacion de una interfaz para DtoolsApiService

---
+++ Rigid/Services/IDtoolsApiService.cs
@@ -0,0 +1,18 @@
+﻿using Rigid.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Rigid.Services
+{
+    public interface IDtoolsApiService
+    {
+
+        //INTERFAZ DE LA API DE DTOOLS
+        Task<List<Billing>> GetBillingAsync(string token);
+        Task PostBillingAsync(Billing billing, string token);
+        Task<List<License>> GetLicensesAsync(string token);
+        Task PostLicenseAsync(License license, string token);
+        Task<List<Document>> GetDocumentsAsync(string token);
+        Task PostDocumentAsync(Document document, string token);
+    }
+}
